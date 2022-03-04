eb27e95426a9907a34232b31e9688ff84d6aed17
GiftOjeabulu <45846624+Gift-Ojeabulu@users.noreply.github.com>
2022-03-04 00:15:20 +0000

    Add files via upload

---
+++ InfobipWhatsapp/send_template_message_basic.py
@@ -0,0 +1,69 @@
+import requests
+
+"""
+ * Send WhatsApp template message directly by calling HTTP endpoint.
+ *
+ * THIS CODE EXAMPLE IS READY BY DEFAULT. HIT RUN TO SEND THE MESSAGE!
+ *
+ * Send WhatsApp API reference: https://www.infobip.com/docs/api#channels/whatsapp/send-whatsapp-template-message
+ *
+ * See Readme file for details.
+"""
+
+BASE_URL = "https://ej23g2.api.infobip.com"
+API_KEY = "App a8a0491b7117dd3242a73b437cc03c88-6f95bc75-1966-4ad1-b5dc-98ac94ccf5af"
+
+SENDER = "447860099299"
+RECIPIENT = "2349077393129"
+
+payload = {
+    "messages":
+        [
+            {
+                "from": SENDER,
+                "to": RECIPIENT,
+                "content": {
+                    "templateName": "registration_success",
+                    "templateData": {
+                      "body": {
+                        "placeholders": [
+                          "sender",
+                          "message",
+                          "delivered",
+                          "testing"
+                        ]
+                      },
+                      "header": {
+                        "type": "IMAGE",
+                        "mediaUrl": "https://api.infobip.com/ott/1/media/infobipLogo"
+                      },
+                      "buttons": [
+                        {
+                          "type": "QUICK_REPLY",
+                          "parameter": "yes-payload"
+                        },
+                        {
+                          "type": "QUICK_REPLY",
+                          "parameter": "no-payload"
+                        },
+                        {
+                          "type": "QUICK_REPLY",
+                          "parameter": "later-payload"
+                        }
+                      ]
+                  },
+                  "language": "en"
+               }
+           }
+        ]
+    }
+
+headers = {
+    'Authorization': API_KEY,
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
+
+response = requests.post(BASE_URL + "/whatsapp/1/message/template", json=payload, headers=headers)
+
+print(response.json())
