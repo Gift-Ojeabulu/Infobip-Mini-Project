c7d21517a57b6c7f8473c583d957e77190b52e75
GiftOjeabulu <45846624+Gift-Ojeabulu@users.noreply.github.com>
2022-03-04 12:07:40 +0100

    Update send_email_basic (1).py

--- InfobipEmail/send_email_basic (1).py
+++ InfobipEmail/send_email_basic (1).py
@@ -2,20 +2,18 @@ import requests
 
 """
  * Send an email message by using Infobip API.
- *
- * This example is already pre-populated with your account data:
- * 1. Your account Base URL
- * 2. Your account API key
- * 3. Your recipient email
- *
- * THIS CODE EXAMPLE IS READY BY DEFAULT. HIT RUN TO SEND THE MESSAGE!
+ 
  *
  * Send email API reference: https://www.infobip.com/docs/api#channels/email/send-email
  * See Readme file for details.
 """
 
+# Note this is just an example as you cannot use this API key, they are meant to be private, this is jus an expmple project.
+# 
+
 BASE_URL = "https://ej23g2.api.infobip.com"
 API_KEY = "App a8a0491b7117dd3242a73b437cc03c88-6f95bc75-1966-4ad1-b5dc-98ac94ccf5af"
+
 
 SENDER_EMAIL = "giftoscar98@selfserviceib.com"
 RECIPIENT_EMAIL = "giftoscar98@gmail.com"
